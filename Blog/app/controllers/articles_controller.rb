a8af9355f96fd76eeedeb102af747fa63588bf77
Paige Sun <paige.sun@cs.mcgill.ca>
2016-08-14 23:20:28 -0400

    Create articles controller and corresponding articles/new.html.erb.

--- Blog/app/controllers/articles_controller.rb
+++ Blog/app/controllers/articles_controller.rb
@@ -1,5 +1,14 @@
 class ArticlesController < ApplicationController
   def new
-    
+  end
+
+  def create
+    # Inspect :article from aricles/new.html.erb, which returns
+      # {"utf8"=>"✓",
+      #  "authenticity_token"=>"J9z55HqXU41wAbzwI...",
+      #  "article"=>{"title"=>"some title", "text"=>"some text"},
+      #  "commit"=>"Save Article"}
+    # params returns an ActionController::Parameters object, allowing for access of keys w strings OR symbols
+    render plain: params[:article].inspect
   end
 end
