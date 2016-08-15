46ea9364f2d3f5fdaeac4dd60c6959bba0225d16
Paige Sun <paige.sun@cs.mcgill.ca>
2016-08-14 22:59:05 -0400

    Generate controller Articles. Add articles/new.html.erb. Add resource :articles.

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   root 'welcome#index'
+
+  # Declare a standard REST resource. i.e. a collection of similar objects.
+  # You can create, read, update and destroy items for a resource (CRUD operations).
+  # $ rails routes           to see the defined REST operations
+  resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
