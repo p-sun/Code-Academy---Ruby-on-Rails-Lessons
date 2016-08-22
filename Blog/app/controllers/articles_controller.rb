a95bdf96b0d0e8be3edae057ef5ce22cc1105f58
Paige Sun <paige.sun@cs.mcgill.ca>
2016-08-21 23:12:15 -0400

    Save data from the articles/new into the create action.

--- Blog/app/controllers/articles_controller.rb
+++ Blog/app/controllers/articles_controller.rb
@@ -3,9 +3,22 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # Use this to inspect :article passed from articles/new.html.erb:
-    #   <ActionController::Parameters {"title"=>"some title", "text"=>"some text"} permitted: false>
-    #   params returns an ActionController::Parameters object, allowing for access of keys w strings OR symbols
-    render plain: params[:article].inspect
+    # Uncomment render to inspect :article passed from articles/new.html.erb:
+      #   <ActionController::Parameters {"title"=>"some title", "text"=>"some text"} permitted: false>
+      #   params returns an ActionController::Parameters object, allowing for access of keys w strings OR symbols
+      #   This can be done before we create a Article model
+    ### render plain: params[:article].inspect
+
+    # After we create an Article model, we can save into the db.
+    @article = Article.new(article_params)
+    @article.save
+    # Redirect user to the show action
+    redirect_to @article
+  end
+
+  # safety feature says that you must whitelist parameters
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
